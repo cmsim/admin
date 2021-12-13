1a48358db9c417403c2fb14e5e9e8cac07ee8009
dxhuii <dxhuii@qq.com>
2021-12-13 22:44:32 +0800

    更新路由

--- src/app.tsx
+++ src/app.tsx
@@ -10,7 +10,7 @@ import { BookOutlined, LinkOutlined } from '@ant-design/icons';
 import { notification } from 'antd';
 
 const isDev = process.env.NODE_ENV === 'development';
-const loginPath = '/user/login';
+const loginPath = '/login';
 
 /** 获取用户信息比较慢的时候会展示一个 loading */
 export const initialStateConfig = {
---
+++ src/pages/user/List/index.tsx
@@ -0,0 +1,5 @@
+const UserList = () => {
+  return <div>userlist</div>;
+};
+
+export default UserList;
